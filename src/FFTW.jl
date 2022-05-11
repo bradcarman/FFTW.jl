5f3eb76df1ab3bdb2a365128f9e79676c492b465
Brad Carman <brad_carman@instron.com>
2022-05-11 08:12:41 -0400

    removed MKL

--- src/FFTW.jl
+++ src/FFTW.jl
@@ -32,8 +32,9 @@ function __init__()
         fftw_init_threads()
     end
     @static if fftw_provider == "mkl"
-        libfftw3[] = MKL_jll.libmkl_rt_path
-        libfftw3f[] = MKL_jll.libmkl_rt_path
+        error("MKL is not available!")
+        # libfftw3[] = MKL_jll.libmkl_rt_path
+        # libfftw3f[] = MKL_jll.libmkl_rt_path
     end
 end
 
